57d69fca45dc3cd572beee7843bffb0b4e75cbc7
Fabio A. Kanashiro <90659464+Fabioak98@users.noreply.github.com>
2021-10-16 13:11:49 -0300

    Customer_Interface
    First implementation

---
+++ src/main/java/com/lm/bank/CustomerMenuController.java
@@ -0,0 +1,35 @@
+package com.lm.bank;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+
+public class CustomerMenuController {
+    @FXML
+    protected void goAddAccount(ActionEvent actionEvent){
+        System.out.println("teste");
+    }
+
+    @FXML
+    protected void goDeposito(ActionEvent actionEvent){
+
+    }
+    @FXML
+    protected void goSacar(ActionEvent actionEvent){
+
+    }
+    @FXML
+    protected void goSaldo(ActionEvent actionEvent){
+
+    }
+    @FXML
+    protected void goExtrato(ActionEvent actionEvent){
+
+    }
+    @FXML
+    protected void goAlteraSenha(ActionEvent actionEvent){
+
+    }
+}
--- src/main/java/com/lm/bank/LoginController.java
+++ src/main/java/com/lm/bank/LoginController.java
@@ -1,24 +1,45 @@
 package com.lm.bank;
 
+import javafx.event.ActionEvent;
+import com.lm.bank.Main;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+
+import java.io.*;
 
 public class LoginController {
+    private Stage stage;
+
     @FXML
     private TextField userInput;
     @FXML
     private PasswordField passwordInput;
+    @FXML
+    private Button loginButton;
 
-    protected void login() {
+    @FXML
+    protected void login(ActionEvent actionEvent) throws IOException{
         String name = userInput.getText();
         String password = passwordInput.getText();
         for (User user : Main.usuario) {
             if (user.authentication(name, password)) {
                 Main.currentUser = user;
+                System.out.println("teste");
                 break;
             }
         }
+        Stage atual = (Stage) loginButton.getScene().getWindow();
+        atual.close();
+
+        FXMLLoader fxmlLoader = new FXMLLoader(LoginController.class.getResource("/CustomerMenu.fxml"));
+        Stage stage = new Stage();
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setScene(scene);
+        stage.show();
+
     }
 }
